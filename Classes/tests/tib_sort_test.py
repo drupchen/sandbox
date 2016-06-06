b355b3c4c22aa3dc20e62dd0f9317edffa011c22
drupchen <hhdrupchen@gmail.com>
2016-06-06 11:52:02 +0200

    icu working

--- Classes/tests/tib_sort_test.py
+++ Classes/tests/tib_sort_test.py
@@ -6,8 +6,12 @@ sys.path.insert(0, path)
 from PyTib import common
 
 
-with open('../PyTib/data/uncompound_lexicon.txt', 'r', -1, 'utf-8-sig') as f:
-    content = [line.strip() for line in f.readlines()]
 
-with open('./files/lexicon.txt', 'w', -1, 'utf-8-sig') as f:
-    f.write('\n'.join(common.tib_sort(content)))
+test = ['བརྐམ', 'ཁ་དོག', 'བསྐལ', 'བསྐོར', 'བསྐྱེད', 'བསྐྱོད', 'ཁ', 'ཁ་ཅིག', 'ཁ་ན་མ་ཐོ', 'བསྐུལ']
+print(common.tib_sort(test))
+
+#with open('../PyTib/data/uncompound_lexicon.txt', 'r', -1, 'utf-8-sig') as f:
+#    content = [line.strip() for line in f.readlines()]
+
+#with open('./files/lexicon.txt', 'w', -1, 'utf-8-sig') as f:
+#    f.write('\n'.join(common.tib_sort(content)))
