b8f4a958fd8dbdca8b0b064b09f3c7531295a11c
drupchen <hhdrupchen@gmail.com>
2016-06-14 19:02:46 +0200

    beginning of TibNgram class

--- Classes/PyTib/common.py
+++ Classes/PyTib/common.py
@@ -4,6 +4,16 @@ from bisect import bisect_left
 from collections import OrderedDict, Callable
 
 
+def filewrite(file_path, content):
+    with open(file_path, 'w', -1, 'utf8') as f:
+        f.write(content)
+
+
+def open_file(file_path):
+    with open(file_path, 'r', -1, 'utf8') as f:
+        return f.read()
+
+
 def search(l, entry, len_l):
     """
     :param l: list on which to apply bisect
@@ -134,3 +144,61 @@ class DefaultOrderedDict(OrderedDict):
     def __repr__(self):
         return 'OrderedDefaultDict(%s, %s)' % (self.default_factory,
                                                OrderedDict.__repr__(self))
+
+
+class PrepareTib:
+    ''' Dealing with the punctuation of Tibetan '''
+
+    def __init__(self, tibstring):
+        self.raw = tibstring.replace('\n', '')  # fast hack to get rid of all returns
+        self.punct = [" ", "༄", "༅", "࿓", "࿔", "༇", "༆", "༈", "།", "༎", "༏", "༐", "༑", "༔","་", "༌", "༼", "༽", "༒", "༓", "ཿ"]
+
+    def gen_tuples(self):
+        syllables = []
+        puncts = []
+        syl = ''
+        for index, char in enumerate(self.raw):
+            if index == 0:
+                if char in self.punct:
+                    puncts.append(char)
+                else:
+                    syl += char
+            else:
+                if char not in self.punct and self.raw[index-1] in self.punct:
+                    syllables.append((syl, puncts))
+                    puncts = []
+                    syl = char
+                elif char not in self.punct:
+                    syl += char
+                elif char in self.punct:
+                    puncts.append(char)
+        # add last syllable + its punctuation
+        if syl != '':
+            syllables.append((syl, puncts))
+        return syllables
+
+    def syl_tuples(self):
+        tuples = PrepareTib.gen_tuples(self)
+        return [(t[0], ''.join(t[1])) for t in tuples]
+
+    def all_punct(self):
+        tuples = PrepareTib.syl_tuples(self)
+        return [t[0]+t[1] for t in tuples]
+
+    def syls_only(self):
+        tuples = PrepareTib.gen_tuples(self)
+        return [t[0] for t in tuples]
+
+    def tsheks_only(self):
+        tuples = PrepareTib.syl_tuples(self)
+        return [t[0]+'་' for t in tuples]
+
+    def no_tshek(self):
+        tuples = PrepareTib.syl_tuples(self)
+        no_tshek = []
+        for t in tuples:
+            if '་' in t[1]:
+                no_tshek.append(t[0]+t[1].replace('་', ''))
+            else :
+                no_tshek.append(t[0]+t[1])
+        return no_tshek
--- Classes/tests/ngram.py
+++ Classes/tests/ngram.py
@@ -3,14 +3,8 @@ import os
 path = os.path.dirname(sys.modules[__name__].__file__)
 path = os.path.join(path, '..')
 sys.path.insert(0, path)
-from PyTib.common import DefaultOrderedDict
+from PyTib.common import DefaultOrderedDict, open_file, filewrite, PrepareTib
 
-
-with open('/home/drupchen/PycharmProjects/sandbox/Classes/new_lexicon/segmented.txt', 'r', -1, 'utf-8-sig') as f:
-    seg = f.read().replace('\n', '').split()
-
-
-# class TibNgram:
 
 
 def find_ngrams(input_list, n):
@@ -31,9 +25,13 @@ def ngrams_by_freq(l, freq=10, min=3, max=12):
     return [(' '.join(n[0]), n[1], len(n[0])) for n in ngrams if n[1] >= freq]
 
 
+
 import time
 A = time.time()
-ngrams = ngrams_by_freq(seg, freq=2, min=20, max=20)
+
+raw = open_file('/home/drupchen/PycharmProjects/sandbox/Classes/new_lexicon/segmented.txt')
+syls = PrepareTib(raw).tsheks_only()
+ngrams = ngrams_by_freq(syls, freq=2, min=20, max=20)
 
 B = time.time()
 print(B-A)
