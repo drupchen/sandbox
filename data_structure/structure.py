22b112d19b6a148a7c7aaa1ca4c1f753075b2eb8
drupchen <hhdrupchen@gmail.com>
2016-06-30 18:01:20 +0200

    MLD works (with difflib)

--- data_structure/structure.py
+++ data_structure/structure.py
@@ -1,13 +1,13 @@
 import re
 import zipfile
 from difflib import ndiff
-from  collections import OrderedDict, defaultdict
+from collections import OrderedDict, defaultdict
 
 
 class MLD:
     def __init__(self, mld=None):
         self.base_string = ''
-        self.layers = defaultdict(dict)
+        self.layers = {}
         self.dependency = []
         self.file_name = ''
         self.metadata = ''
@@ -46,20 +46,64 @@ class MLD:
                 temp[idx] = index
 
             if temp[idx] == '' or index == temp[idx]:
-                if operation == '+':
+                if operation == 'ᛯ':
                     if temp[val] == '':
-                        temp[val] = modified+'_'
+                        temp[val] = modified+'ᛝ'
                     else:
                         temp[val] = temp[val][:-1]+modified+temp[val][-1]
-                elif operation == '=':
+                elif operation == 'ᛞ':
                     temp[val] = temp[val][:-1] + modified
-                elif operation == '-':  # assumes that there is at least one character in the string
-                    temp[val] = temp[val][:-1]+ '-'
+                elif operation == 'ᛰ':  # assumes that there is at least one character in the string
+                    temp[val] = temp[val][:-1]+ 'ᛰ'
             flattened[temp[0]] = temp[1]
         return flattened
 
-    def import_layer(self, name, layer_string):
-        self.layers[name] = layer_string
+    def create_layer(self, layer_name, modified):
+        """
+        creates a layer using difflib.ndiff()
+        :param base: base string
+        :param modified: modified string
+        :return:
+        """
+        # make a diff of both strings with ndiff
+        diff = ndiff(self.base_string, modified)
+        # list all changes, either additions or deletions
+        layer = []
+        c = 0
+        for line in diff:
+            operation = line[0] + line[2:]
+            # only increment the counter when there is no modification in the current line
+            if line.startswith(' '):
+                c += 1
+            # append the modification found in the current line
+            else:
+                if line.startswith('+'):
+                    layer.append([c, 'ᛯ'+operation[1:]])
+                else:
+                    # only increment the counter for deletions
+                    layer.append([c, 'ᛰ'+operation[1:]])
+                    c += 1
+        # find replacements and apply them
+        c = 0
+        while c < len(layer)-1:
+            # if current and next operations are on the same index
+            if layer[c][0] == layer[c+1][0]:
+                # if there is + followed by -
+                if 'ᛯ' in layer[c][1] and 'ᛰ' in layer[c+1][1]:
+                    layer[c][1] = layer[c][1].replace('ᛯ', 'ᛞ')
+                    del layer[c+1]
+                    c += 1
+                # if there is - followed by +
+                elif 'ᛰ' in layer[c][1] and 'ᛯ' in layer[c+1][1]:
+                    del layer[c]
+                    layer[c][1] = layer[c][1].replace('ᛯ', 'ᛞ')
+                    c += 1
+                else:
+                    c += 1
+            else:
+                c += 1
+        # join the tuples with \t and return the list
+        self.layers[layer_name] = '\n'.join([str(i[0])+'\t'+i[1] for i in layer])
 
     def merge_layers(self, layers):
         merged = {}
@@ -84,9 +128,9 @@ class MLD:
                 temp = char
                 if str(num) in merged_layers:
                     modif = merged_layers[str(num)]
-                    # if modif is only 1 char, it can either be a deletion ('-') or a replacement
+                    # if modif is only 1 char, it can either be a deletion ('ᛰ') or a replacement
                     if len(modif) == 1:
-                        if modif == '-':
+                        if modif == 'ᛰ':
                             # delete the current char
                             temp = ''
                         else:
@@ -94,19 +138,23 @@ class MLD:
                             temp = modif
                     # if it has more than a char, 1. either an addition (xxx_), or 2. an addition and a deletion (xxx-) or 3. an addition and a replacement (xxxy)
                     # 1. add modif before the current index char
-                    elif modif.endswith('_'):
+                    elif modif.endswith('ᛝ'):
                         temp = temp[:-1]+modif[:-1]+temp[-1]
                     # 2. delete current index char
-                    elif modif.endswith('-'):
+                    elif modif.endswith('ᛰ'):
                         temp = temp[:-1]+modif[:-1]
                     # 3. apply the addition and replace the current index char
                     else:
                         temp = temp[:-1]+modif
                 view += temp
+            # add the last element in merged_layers if there is one
+            last_elt = str(int(num)+1)
+            if last_elt in merged_layers:
+                view += merged_layers[last_elt][:-1]
             return view
 
-    def write_mld(self):
-        with zipfile.ZipFile(self.file_name+'.mld', 'w', zipfile.ZIP_DEFLATED) as z:
+    def write_mld(self, file_name):
+        with zipfile.ZipFile(file_name+'.mld', 'w', zipfile.ZIP_DEFLATED) as z:
             z.writestr('base_string', self.base_string)
             for l in self.layers:
                 z.writestr(l, self.layers[l])
@@ -126,33 +174,31 @@ class MLD:
             return 'non-valid file'
 
 
-def create_layer(base, modified):
-    diff = ndiff(base, modified)
-    layer = []
+def html_strip(html):
+    # delete everything that precedes <body>
+    body = html.split('</head>')[1]
+    html_content = ''
     c = 0
-    for line in diff:
-        operation = line[0] + line[2:]
-        # turn - followed by + operation into a replace operation(=)
-        if layer != [] and '-' in layer[-1] and '+' in line:
-            parts = layer[-1].split('\t')
-            layer[-1] = parts[0] + '\t' + parts[1][:-2] + '=' + operation[-1]
+    while c <= len(body)-1:
+        if body[c-1] == '>' and body[c] != '<':
+            while body[c] != '<':
+                html_content += body[c]
+                c += 1
         else:
-            # only increment the counter when there is no modification in the current line
-            if line.startswith('  '):
-                c += 1
-            # append the modification found in the current line
-            else:
-                layer.append(str(c) + '\t' + operation)
-                c += 1
-    print('__'.join(layer))
-    return '\n'.join(layer)
+            c += 1
+    return html_content
 
-test = MLD('a b c d')
-test.import_layer('l1', create_layer(test.export_view(), 'aA bB cC dD'))
+qhtml = '''<html><head><meta name="qrichtext" content="1" /><style type="text/css">p, li { white-space: pre-wrap; }</style></head><body style=" font-family:'MS Shell Dlg 2'; font-size:7.8pt; font-weight:400; font-style:normal;"><p style=" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;"><span style=" font-size:14pt; font-style:italic;">༄༅། །བླ་མ་དཀོན་</span><span style=" font-size:14pt; color:#005500;">མཆོག་རྩ</span><span style=" font-size:14pt; text-decoration: underline; color:#005500;">་</span><span style=" font-size:14pt;">གསུམ་ཐུགས་རྗེ་ཡི</span><span style=" font-size:14pt; font-weight:600;">་བདག་ཉིད།།</span></p></body></html>'''
+qhtml = qhtml.replace('\n', '')
+content = html_strip(qhtml)
+
+test = MLD(content)
+test.create_layer('markup', qhtml)
+test.create_layer('l1', '༄༅། །བླ་མ་ དཀོན་མཆོག་ རྩ་ གསུམ་ ཐུགས་རྗེ་ ཡི་ བདག་ཉིད།།')
+print(test.export_view())
 print(test.export_view(layers='l1'))
+print(test.export_view(layers='l1+markup'))
 
-
-#print(MLD('./test.mld'))
 
 
 '''
@@ -163,9 +209,9 @@ Layers:
         output : flattens all the operations into a single string following this scheme:
                     z   : replace the char at current index with z
                     xyz : replace the char at current index by z and add xy before the char
-                    xy_ : keep the char at current index and add xy before it
-                    xy- : delete the char at current index and add xy before it
-                    -   : delete the char at current index
+                    xyᛝ : keep the char at current index and add xy before it
+                    xyᛰ : delete the char at current index and add xy before it
+                    ᛰ   : delete the char at current index
 
     create layer()
         input : - processed base string that contains +, - or = signs and name for the layer
@@ -192,10 +238,6 @@ Output:
         The output string is the base string on which the given layers have been applied.
         the raw string is given if layers equals ''
 
-    cache_indexed(suffix, layers)
-        the output is the the same string than export_flattened(), but together with the indexes
-        of each character referring to the base string.
-
 
 Metadata:
 
@@ -210,9 +252,9 @@ Metadata:
 '''
 '''
 layer layout :
-    index\t+char    # add a character before index
-    index\t-        # deletes the character at index
-    index\t=char    # replaces the character at index
+    index\tᛯchar    # add a character before index
+    index\tᛰ        # deletes the character at index
+    index\tᛞchar    # replaces the character at index
 '''
 
 
